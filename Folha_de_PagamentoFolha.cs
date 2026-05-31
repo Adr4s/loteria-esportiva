466837e068db6784f6db7cf61207e4bd0416fbf6
Adr4s <adrassantos648@gmail.com>
2026-05-31 11:46:12 -0300

    Remove redundant code in Folha_de_PagamentoFolha.cs

--- Folha_de_PagamentoFolha.cs
+++ Folha_de_PagamentoFolha.cs
@@ -14,14 +14,14 @@ class Program
         double hora = salarioClasse1 / horasMes;
         double extra = hora * 1.3;
 
-        Console.Write("Digite quantos funcionários: ");
+        Console.Write("Digite quantos funcionarios: ");
         int n = int.Parse(Console.ReadLine());
 
         for (int i = 0; i < n; i++)
         {
-            Console.WriteLine("\nFuncionário " + (i + 1));
+            Console.WriteLine("\nFuncionario " + (i + 1));
 
-            Console.Write("Inscrição: ");
+            Console.Write("Inscricao: ");
             int inscricao = int.Parse(Console.ReadLine());
 
             Console.Write("Nome: ");
@@ -43,12 +43,12 @@ class Program
             double liquido = bruto - inss;
 
             Console.WriteLine("\nContra-cheque");
-            Console.WriteLine("Inscrição: " + inscricao);
+            Console.WriteLine("Inscricao: " + inscricao);
             Console.WriteLine("Nome: " + nome);
-            Console.WriteLine("Salário horas normais: " + totalNormal.ToString("C"));
-            Console.WriteLine("Salário horas extras: " + totalExtra.ToString("C"));
+            Console.WriteLine("Salario horas normais: " + totalNormal.ToString("C"));
+            Console.WriteLine("Salario horas extras: " + totalExtra.ToString("C"));
             Console.WriteLine("INSS: " + inss.ToString("C"));
-            Console.WriteLine("Salário líquido: " + liquido.ToString("C"));
+            Console.WriteLine("Salario liquido: " + liquido.ToString("C"));
         }
     }
 }
